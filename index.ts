47bcf2cea7879a74017f7ffc335bb467e13465f7
Habiba-Wasim <160347457+Habiba-Wasim@users.noreply.github.com>
2024-02-23 21:08:51 +0500

    Step13 Of Typescript
    Step13 Of Typescript

---
+++ index.ts
@@ -0,0 +1,5 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+let vehichles: string[] = ["Honda motorcycle", "Honda city", "Hilux", "Audi"];
+
+vehichles.map((items) => console.log(`I would like to own a ${items}`)); 
